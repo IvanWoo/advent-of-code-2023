cb82df78f68e2977ad19c1e5516950e7fb364676
Yifan Wu <tyivanwu@gmail.com>
2023-12-06 00:40:59 -0500

    feat: 06

---
+++ 06/main.py
@@ -0,0 +1,81 @@
+import fileinput
+from pathlib import Path
+from functools import reduce
+from math import floor, ceil
+
+from sympy import symbols, solve
+
+ROOT_DIR = Path(__file__).resolve().parents[0]
+INPUT_FILE = ROOT_DIR / "input.txt"
+
+x, t, d = symbols("x t d")
+
+
+def get_solutions(t: int, d: int):
+    solutions = solve(x**2 - t * x + d, x)
+    return [ceil(min(solutions).evalf()), floor(max(solutions).evalf())]
+
+
+def get_input():
+    with fileinput.input(files=(INPUT_FILE)) as f:
+        for line in f:
+            yield line.strip()
+
+
+def parse_1():
+    vals = []
+    for line in get_input():
+        vals.append([int(x) for x in line.split(":")[-1].split()])
+    return vals
+
+
+def parse_2():
+    vals = []
+    for line in get_input():
+        vals.append(int("".join([x for x in line.split(":")[-1].split()])))
+    return vals
+
+
+def q1():
+    times, distances = parse_1()
+    n = len(times)
+    counts = []
+    for i in range(n):
+        count = 0
+        for hold in range(i, times[i]):
+            if hold * (times[i] - hold) > distances[i]:
+                count += 1
+        counts.append(count)
+    return reduce(lambda a, b: a * b, counts, 1)
+
+
+# 2.5s
+def _q2():
+    time, distance = parse_2()
+    count = 0
+    for hold in range(time):
+        if hold * (time - hold) > distance:
+            count += 1
+    return count
+
+
+# 523.28 millis
+def _q2_fast():
+    time, distance = parse_2()
+    solutions = get_solutions(time, distance)
+    return min(time, solutions[1]) - solutions[0] + 1
+
+
+def q2(fast=True):
+    return _q2_fast() if fast else _q2()
+
+
+def main():
+    print(q1())
+    print(q2())
+    assert q1() == 6209190
+    assert q2() == 28545089
+
+
+if __name__ == "__main__":
+    main()
